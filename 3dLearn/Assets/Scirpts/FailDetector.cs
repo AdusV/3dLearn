e39a44472e9885e4613827a2ba685caebcbbcd0e
AdusV <adriankomendera@interia.pl>
2020-06-24 21:58:37 +0200

    Stworzenie bariery,dodanie zyc, zapisywanie zyc Set int,odczytywanie aktualnych zyc GetInt

---
+++ 3dLearn/Assets/Scirpts/FailDetector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FailDetector : MonoBehaviour
+{
+    public GameObject panelGameOver;
+    public LivesController livesController;
+   private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (livesController.lives > 0)
+            {
+                livesController.lives -= 1;
+                //Set sluzy w celu zapmietania 
+                PlayerPrefs.SetInt("lives", livesController.lives);
+                livesController.UpdateLives();
+                StartCoroutine(BeginStart());
+
+            }
+            else
+            {
+                panelGameOver.SetActive(true);
+                livesController.ResetLives();
+
+                StartCoroutine(BeginStart());
+            }
+            
+        }
+      
+        IEnumerator BeginStart()
+        {
+            yield return new WaitForSeconds(3f);
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+}
---
+++ 3dLearn/Assets/Scirpts/LivesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesController : MonoBehaviour
+{
+    public int lives;
+    public Text livesText;
+    private void Start()
+    {
+            if(PlayerPrefs.HasKey("lives"))
+        {//Get sluzy w celu odczytania
+            lives = PlayerPrefs.GetInt("lives");
+            UpdateLives();
+        }
+            else
+        {
+            lives = 3;
+            UpdateLives();
+        }
+    }
+    public void UpdateLives()
+    {
+        livesText.text = "Życia: " + lives.ToString();
+    }
+    public void ResetLives()
+    {
+        lives = 3;
+        PlayerPrefs.SetInt("lives", lives);
+    }
+}
